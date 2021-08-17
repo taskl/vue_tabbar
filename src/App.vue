7fc28796c8d3b3391d00aa38cd213deb39fa8120
webcnt <76040363+taskl@users.noreply.github.com>
2021-08-17 14:52:39 +0800

    vue的tabbar封装

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+     <router-view></router-view>
+      <maintabbar></maintabbar>
+
+  </div>
+</template>
+
+<script>
+ // import tabbar from './components/tabbar/tabbar.vue'
+ // import tabbarltem from './components/tabbar/tabbarltem.vue'
+import Maintabbar from './components/Maintabbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+      Maintabbar
+  }
+}
+</script>
+
+<style>
+  @import url("./assets/css/base.css");
+
+</style>
---
+++ src/components/Maintabbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <tabbar>
+     <tabbarltem path="/home" activeColor="deepPink">
+        <img slot="item-icon"  src="../assets/img/tabbar/home.svg" alt=""/>
+        <img  slot="item-icon-active" src="../assets/img/tabbar/home_active.svg"/>
+        <div slot="item-text">首页</div>
+      </tabbarltem>
+      <tabbarltem path="/category" activeColor="deepPinkdeepPink">
+        <img  slot="item-icon" src="../assets/img/tabbar/category.svg" alt=""/>
+        <img  slot="item-icon-active" src="../assets/img/tabbar/category_active.svg"/>
+        <div slot="item-text">分类</div>
+      </tabbarltem>
+      <tabbarltem path="/cart" activeColor="deepPink">
+        <img  slot="item-icon" src="../assets/img/tabbar/shopcart.svg" alt=""/>
+        <img  slot="item-icon-active" src="../assets/img/tabbar/shopcart_active.svg"/>
+        <div slot="item-text">购物车</div>
+      </tabbarltem>
+      <tabbarltem path="/profle" activeColor="deepPink">
+        <img  slot="item-icon" src="@/assets/img/tabbar/profile.svg" alt=""/>
+        <img  slot="item-icon-active" src="@/assets/img/tabbar/profile_active.svg"/>
+        <div slot="item-text">我的</div>
+      </tabbarltem>
+    </tabbar>
+</template>
+
+<script>
+  import Tabbar from './tabbar/Tabbar.vue'
+  import Tabbarltem from './tabbar/Tabbarltem.vue'
+
+
+  export default{
+    name:'Maintabbar',
+    components: {
+      Tabbar,
+      Tabbarltem
+    }
+  }
+</script>
+
+<style>
+</style>
